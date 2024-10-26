394cedb907203dd2573faeaf3abeb56b124fbb17
Thapelo Tsotetsi <tsotetsithapelo2030@gmail.com>
2024-10-26 07:22:23 +0200

    Add mailit for user email testing. closes #10

--- config/settings/local.py
+++ config/settings/local.py
@@ -12,3 +12,10 @@ SECRET_KEY = "nJF7857vESYkIayySmQySmvPmumtfsttzIRhgy1bOvf3gNaYDeqW2G7Hvv7qAxxa"
 
 # https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
+
+# EMAIL
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
+EMAIL_HOST = os.getenv("EMAIL_HOST", default="mailpit")
+# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
+EMAIL_PORT = 1025
